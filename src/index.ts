dd70659460726b48765563eac4c7d59f981a5726
boby900 <bobyt2265@gmail.com>
2024-12-13 16:42:25 +0530

    did some refactoring

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import connectDB from './db';
+import connectDB from './db.js';
 dotenv.config({ path: '.env' });
 
 
